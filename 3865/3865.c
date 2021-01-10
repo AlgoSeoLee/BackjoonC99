fa482a1ec16c5d2fe35cc5368af3316b7bc65309
tpdns90321 <tpdns9032100@gmail.com>
2021-01-10 07:11:26 +0000

    백준 3865 학회원 문제 섦명 예제와 기본 입출력 작동
    근데 실패ㅋ

--- 3865/3865.c
+++ 3865/3865.c
@@ -2,67 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct _Node {
-	void* data;
-	void* next;
-} Node;
-
 typedef struct _Tree {
 	char* name;
-	Node* child;
+	void* sibiling;
+	void* child;
 } Tree;
+
+void InitNode(Tree* tree, char* name) {
+	size_t name_length = strlen(name);
+	tree->name = (char*)malloc(name_length * sizeof(char));
+	strcpy(tree->name, name);
+	tree->sibiling = NULL;
+	tree->child = NULL;
+}
+
+Tree* CreateNode(char* name) {
+	Tree* tree = (Tree*)malloc(sizeof(Tree));
+	InitNode(tree, name);
+	return tree;
+}
 
 typedef enum _TreeIterState {
 	KEEP = 0,
 	RETURN,
 } TreeIterState;
 
-typedef TreeIterState (*TreeIterFunc)(Tree*, void*, int);
+typedef struct _TreeIterInfo {
+	void* arg;
+	Tree* top;
+	Tree* prev;
+} TreeIterInfo;
 
-void InitTree(Tree* tree, char* name) {
-	size_t name_length = strlen(name);
-	tree->name = (char*)calloc(name_length, sizeof(char));
-	strcpy(tree->name, name);
-	tree->child = NULL;
-}
-
-Node* CreateNode(char* name) {
-	Tree* tree = (Tree*)malloc(sizeof(Tree));
-	InitTree(tree, name);
-	Node* node = (Node*)malloc(sizeof(Node));
-	node->data = tree;
-	node->next = NULL;
-
-	return node;
-}
+typedef TreeIterState (*TreeIterFunc)(Tree*, TreeIterInfo*, int);
 
 typedef enum _IsLeaf {
 	LEAF,
 	NON_LEAF
 } IsLeaf;
 
-Tree* IterateTree(Tree* tree, TreeIterFunc work, void* arg, TreeIterState* state) {
-	Node* cur = tree->child;
+Tree* IterateTree(Tree* tree, TreeIterFunc work, TreeIterInfo info, TreeIterState* state) {
+	Tree* cur = tree->child;
+	TreeIterInfo newInfo;
+	newInfo.arg = info.arg;
+	newInfo.top = tree;
+	newInfo.prev = NULL;
 	int is_leaf = LEAF;
 	while (cur != NULL) {
 		is_leaf = NON_LEAF;
-		Tree *next = cur->data;
-		Tree *result = IterateTree(next, work, arg, state);
+		Tree *result = IterateTree(cur, work, newInfo, state);
 		if (state != NULL) {
 			if (*state == RETURN)
 				return result;
 		}
-		cur = cur->next;
+		newInfo.prev = cur;
+		cur = cur->sibiling;
 	}
 
 	if (state != NULL)
-		*state = work(tree, arg, is_leaf);
+		*state = work(tree, &info, is_leaf);
 	else
-		work(tree, arg, is_leaf);
+		work(tree, &info, is_leaf);
 	return tree;
 }
 
-TreeIterState _FindByNameTree(Tree* tree, void* arg, int is_leaf) {
+TreeIterState _FindByNameTree(Tree* tree, TreeIterInfo* info, int is_leaf) {
+	void* arg = info->arg;
 	if (strcmp((char*)arg, tree->name) == 0)
 		return RETURN;
 
@@ -71,14 +75,44 @@ TreeIterState _FindByNameTree(Tree* tree, void* arg, int is_leaf) {
 
 Tree* FindByNameTree(Tree* tree, char* name) {
 	TreeIterState state;
-	Tree* result = IterateTree(tree, _FindByNameTree, name, &state);
+	TreeIterInfo info;
+	info.arg = name;
+	Tree* result = IterateTree(tree, _FindByNameTree, info, &state);
 	if (state == KEEP)
 		return NULL;
 
 	return result;
 }
 
-TreeIterState _CountLeafNodeTree(Tree* tree, void* arg, int is_leaf) {
+TreeIterState _DeleteByNameTree(Tree* tree, TreeIterInfo* info, int is_leaf) {
+	void* arg = info->arg;
+	Tree* prev = info->prev;
+	if (strcmp((char*)arg, tree->name) == 0) {
+		if (prev != NULL) {
+			prev->sibiling = tree->sibiling;
+		} else {
+			info->top->child = tree->sibiling;
+		}
+		return RETURN;
+	}
+
+	return KEEP;
+}
+
+Tree* DeleteByNameTree(Tree* tree, char* name) {
+	TreeIterState state;
+	TreeIterInfo info;
+	info.top = tree;
+	info.arg = name;
+	Tree* result = IterateTree(tree, _DeleteByNameTree, info, &state);
+	if (state == KEEP)
+		return NULL;
+
+	return result;
+}
+
+TreeIterState _CountLeafNodeTree(Tree* tree, TreeIterInfo* info, int is_leaf) {
+	void* arg = info->arg;
 	if (is_leaf == LEAF) {
 		int* leaves = arg;
 		(*leaves)++;
@@ -89,57 +123,71 @@ TreeIterState _CountLeafNodeTree(Tree* tree, void* arg, int is_leaf) {
 
 int CountLeafNodeTree(Tree* tree) {
 	int leaves = 0;
-	IterateTree(tree, _CountLeafNodeTree, &leaves, NULL);
+	TreeIterInfo info;
+	info.arg = &leaves;
+	IterateTree(tree, _CountLeafNodeTree, info, NULL);
 	return leaves;
 }
 
 Tree* InsertNodeTree(Tree* tree, char* name) {
 	Tree* result = NULL;
-	Node* child = tree->child;
+	Tree* child = tree->child;
 
 	if (child == NULL) {
 		child = CreateNode(name);
-		result = (Tree*)child->data;
+		result = child;
+		tree->child = child;
 	} else {
-		child->next = CreateNode(name);
-		result = (Tree*)((Node*)child->next)->data;
+		Tree* last = child;
+		while (last->sibiling != NULL)
+			last = last->sibiling;
+		last->sibiling = CreateNode(name);
+		result = last->sibiling;
 	}
 
-	tree->child = child;
 	return result;
 }
 
-TreeIterState _FreeTree(Tree *tree, void* args, int is_leaf) {
+TreeIterState _FreeTree(Tree *tree, TreeIterInfo* info, int is_leaf) {
 	free(tree->name);
-	free(tree->child);
+	free(tree);
+	return KEEP;
 }
 
 void FreeTree(Tree *tree) {
-	IterateTree(tree, _FreeTree, NULL, NULL);
-	free(tree);
+	TreeIterInfo info;
+	IterateTree(tree, _FreeTree, info, NULL);
+}
+
+TreeIterState _PrintTree(Tree *tree, TreeIterInfo* info, int is_leaf) {
+	printf("%s ", tree->name);
+	return KEEP;
+}
+
+void PrintTree(Tree *tree) {
+	TreeIterInfo info;
+	IterateTree(tree, _PrintTree, info, NULL);
+	printf("\n");
 }
 
 int main() {
 	while (1) {
 		int c;
-		Tree *tree = (Tree*)malloc(sizeof(Tree));
-		InitTree(tree, "ROOT");
+		Tree *tree = CreateNode("ROOT");
+		Tree *academy = NULL;
 
 		scanf("%d", &c);
 		if (c == 0) {
 			break;
 		}
 
-		Tree* academy;
-
 		int i;
-		char line[160];
+		char line[161];
 		for(i = 0; i < c; i++) {
 			scanf("%s", line);
 			char *word = strtok(line, ":,.");
 			if (i == 0) {
-				InsertNodeTree(tree, word);
-				academy = tree->child->data;
+				academy = InsertNodeTree(tree, word);
 				word = strtok(NULL, ":,.");
 				while (word != NULL) {
 					InsertNodeTree(academy, word);
@@ -147,21 +195,31 @@ int main() {
 				}
 			} else {
 				Tree* subAcademy = FindByNameTree(academy, word);
-				if (subAcademy != NULL) {
-					word = strtok(NULL, ":,.");
-					while (word != NULL) {
-						Tree *duplicate = FindByNameTree(academy, word);
-						if (duplicate == NULL) {
-							InsertNodeTree(subAcademy, word);
-						}
-						word = strtok(NULL, ":,.");
+				int is_main = 1;
+				if (subAcademy == NULL) {
+					subAcademy = InsertNodeTree(tree, word);
+					is_main = 0;
+				}
+
+				word = strtok(NULL, ":,.");
+				while (word != NULL) {
+					Tree *duplicate =
+						is_main ? DeleteByNameTree(tree,word):FindByNameTree(tree, word);
+					if (duplicate == NULL) {
+						InsertNodeTree(subAcademy, word);
+					} else if (is_main) {
+						Tree* node = InsertNodeTree(subAcademy, word);
+						node->child = duplicate->child;
+						free(duplicate);
 					}
+					word = strtok(NULL, ":,.");
 				}
 			}
 		}
 
+		PrintTree(academy);
 		printf("%d\n", CountLeafNodeTree(academy));
-		FreeTree(academy);
+		FreeTree(tree);
 	}
 
 	return 0;
