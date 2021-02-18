bcfa5b0044a123de0af22de2955d82c0c4d80934
tpdns90321 <tpdns9032100@gmail.com>
2021-02-18 10:59:07 +0000

    failed 1485 #2

--- 1485/1485.c
+++ 1485/1485.c
@@ -1,32 +1,65 @@
 #include <stdio.h>
+#include <math.h>
+
+#define NON_ZERO(a) a != 0 ? a : 1;
 
 int main() {
 	int c;
 	scanf("%d", &c);
 
+	int points[4][2];
+
 	while (c--) {
-		int startX = 0, startY = 0;
-		scanf("%d %d", &startX, &startY);
+		int i;
+		for (i = 0; i < 4; i++) {
+			int* p = points[i];
+			scanf("%d %d", p, p+1);
+		}
 
-		int length = 0;
 		int invalid = 0;
-		int x, y, i;
-		for (i = 0; i < 3; i++) {
-			scanf("%d %d", &x, &y);
-			int diff = 0;
-			if (startX != x)
-				diff = startY > y ? startY - y : y - startY;
-			else
-				diff = startX > x ? startX - x : x - startX;
 
-			if (length == 0)
-				length = diff;
-			else if (length != diff) {
-				invalid = 1;
-				break;
+		int* a = points[0];
+		int* b = points[1];
+		int* c = points[2];
+
+		float abTx = NON_ZERO(b[0] - a[0]);
+		float bcTx = NON_ZERO(c[0] - b[0]);
+
+		float abTy = NON_ZERO(b[1] - a[1]);
+		float bcTy = NON_ZERO(c[1] - b[1]);
+
+		int rSlopeAB = -(abTy / abTx);
+		int SlopeBC = bcTx / bcTy;
+		if (rSlopeAB != SlopeBC) {
+			invalid = 1;
+		} else {
+			int length = 0;
+			for (i = 1; i < 4; i++) {
+				int* from = points[i - 1];
+				int* to = points[i];
+
+				int tx2 = pow(to[0] - from[0], 2);
+				int ty2 = pow(to[1] - from[1], 2);
+
+				int line = tx2 + ty2;
+				if (from[0] == from[0])
+					line = ty2;
+				else if (from[1] == from[1])
+					line = tx2;
+				else
+					line = tx2 + ty2;
+
+				if (length == 0)
+					length = line;
+				else if (length != line) {
+					invalid = 1;
+					break;
+				}
 			}
 		}
 
 		printf("%d\n", invalid == 0 ? 1 : 0);
 	}
+
+	return 0;
 }
