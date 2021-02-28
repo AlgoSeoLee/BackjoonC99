05167deb7432fe61bde4346176cf60f882cbb841
tpdns90321 <tpdns9032100@gmail.com>
2021-02-28 14:55:29 +0000

    Solve 1485

--- 1485/1485.c
+++ 1485/1485.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <math.h>
+#include <limits.h>
 
-#define NON_ZERO(a) a != 0 ? a : 1;
+// https://www.acmicpc.net/problem/1485 정사각형 <실버 4>
 
 int main() {
 	int c;
@@ -16,46 +17,60 @@ int main() {
 			scanf("%d %d", p, p+1);
 		}
 
-		int invalid = 0;
-
-		int* a = points[0];
-		int* b = points[1];
-		int* c = points[2];
-
-		float abTx = NON_ZERO(b[0] - a[0]);
-		float bcTx = NON_ZERO(c[0] - b[0]);
-
-		float abTy = NON_ZERO(b[1] - a[1]);
-		float bcTy = NON_ZERO(c[1] - b[1]);
-
-		int rSlopeAB = -(abTy / abTx);
-		int SlopeBC = bcTx / bcTy;
-		if (rSlopeAB != SlopeBC) {
-			invalid = 1;
-		} else {
-			int length = 0;
-			for (i = 1; i < 4; i++) {
-				int* from = points[i - 1];
-				int* to = points[i];
+		int invalid = 0, min = INT_MAX;
+		char origin[2], originI = 0;
+		for (i = 0; i < 4; i++) {
+			int j;
+			char far = 0;
+			for (j = 0; j < 4; j++) {
+				if (i == j)
+					continue;
+				int* from = points[i];
+				int* to = points[j];
 
 				int tx2 = pow(to[0] - from[0], 2);
 				int ty2 = pow(to[1] - from[1], 2);
 
 				int line = tx2 + ty2;
-				if (from[0] == from[0])
-					line = ty2;
-				else if (from[1] == from[1])
-					line = tx2;
-				else
-					line = tx2 + ty2;
 
-				if (length == 0)
-					length = line;
-				else if (length != line) {
-					invalid = 1;
-					break;
+				if (line < min) {
+					min = line;
+					if (i == 0) {
+						origin[0] = j;
+						if (originI == 0)
+							originI++;
+						else
+							far++;
+					}
+				} else if (line > min)
+					far++;
+				else {
+					if (i == 0)
+						origin[originI] = j;
 				}
 			}
+
+			if (far != 1) {
+				invalid = 1;
+				break;
+			}
+		}
+
+		if (!invalid) {
+			int* a = points[origin[0]];
+			int* b = points[0];
+			int* c = points[origin[1]];
+
+			float abTx = b[0] - a[0];
+			float bcTx = c[0] - b[0];
+
+			float abTy = b[1] - a[1];
+			float bcTy = c[1] - b[1];
+
+			int rSlopeAB = -(abTy / abTx);
+			int SlopeBC = bcTx / bcTy;
+			if (rSlopeAB != SlopeBC)
+				invalid = 1;
 		}
 
 		printf("%d\n", invalid == 0 ? 1 : 0);
